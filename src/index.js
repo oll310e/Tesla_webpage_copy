6f4ce1a0d82b62f79b930cb7757d804839da482a
oll310e <o.anderzen@gmail.com>
2023-03-08 09:05:29 +0100

    Fix scroll-snap

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./style.css";
+import scrollToNearestPicture from "./background-scroll.js";
+
+// let isScrolling;
+// window.addEventListener("scroll", () => {
+//   clearTimeout(isScrolling);
+//   isScrolling = setTimeout(() => {
+//     scrollToNearestPicture();
+//   }, 500);
+// });
